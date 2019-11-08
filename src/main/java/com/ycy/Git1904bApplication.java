cf82ca9834252529cfdfba2e50c492f5f66a91a9
ycy990130 <1835801084@qq.com>
2019-11-08 19:05:30 +0800

    aaa

---
+++ src/main/java/com/ycy/Git1904bApplication.java
@@ -0,0 +1,13 @@
+package com.ycy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git1904bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git1904bApplication.class, args);
+    }
+
+}
